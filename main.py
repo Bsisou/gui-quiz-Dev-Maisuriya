1bff5d3fd7571d01fd8c6664457d36f8f91aea58
Dev-Maisuriya <168686445+Dev-Maisuriya@users.noreply.github.com>
2024-05-21 23:45:51 +0000

    Added background image

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import tkinter as tk
+from PIL import Image, ImageTk
+
+#5D088E is the primary purple colour for my quiz images
 
 window = tk.Tk()
 window.title("Quiz of Vexatious Contemplation")
-window.geometry("300x300")
+window.geometry("634x469")
+window.minsize(634, 469)
 
-hello = tk.Label(text="Quiz of Vexatious Contemplation")
-hello.pack()
-button = tk.Button(text="Start")
-button.pack()
+bg_image = Image.open("enternamepage.png")
+bg_image = ImageTk.PhotoImage(bg_image)
+
+quiz_frame = tk.Canvas(bg="whitesmoke")
+quiz_frame.pack(expand=True, fill=tk.BOTH)
+
+quiz_frame.create_image(0, 0, image=bg_image, anchor="nw")
+
+
+
+
 
 tk.mainloop()
