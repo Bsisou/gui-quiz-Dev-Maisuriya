4f99b89d618838872c6a0c029ccdeeaefa273d51
Dev-Maisuriya <168686445+Dev-Maisuriya@users.noreply.github.com>
2024-06-04 01:00:22 +0000

    placed image for starting page and set up relative scaling of image for window size

--- main.py
+++ main.py
@@ -9,17 +9,16 @@ class NameEnter:
     def __init__(self, parent):
         self.quiz_canvas = tk.Canvas(parent, bg="#290f42")
         self.quiz_canvas.pack(expand=True, fill=tk.BOTH)
-        self.bg_image = ImageTk.PhotoImage(Image.open("enternamepage.png"))
 
         bg_entry_colour = "#5D088E"
         self.enter_name = tk.Entry(self.quiz_canvas, bg=bg_entry_colour, highlightthickness=0, foreground="#edd818", font=("Arial Narrow", 18, "bold"))
         self.enter_name.pack(anchor=tk.CENTER)
         
+        self.bg_image = ImageTk.PhotoImage(Image.open("enternamepage.png"))
         self.quiz_canvas.create_image(0, 0, image=self.bg_image, anchor="nw")
-        
         window.update()
-
         self.on_resize()
+
         
         self.quiz_canvas.bind("<Configure>", self.on_resize)
         self.enter_name.bind("<Return>", self.name_storage)
@@ -69,13 +68,46 @@ class NameEnter:
 class StartPage:
     def __init__(self, parent):
         print("StartPage works")
-        self.a = tk.Canvas(parent, bg="#290f42")
-        self.a.pack(side="top", expand=True, fill=tk.BOTH)
+        self.quiz_canvas = tk.Canvas(parent, bg="#290f42")
+        self.quiz_canvas.pack(side="top", expand=True, fill=tk.BOTH)
         self.bg_image = ImageTk.PhotoImage(Image.open("resizedstarterpagev2.png"))
 
-        self.a.create_image(0, 0, image=self.bg_image, anchor="nw")
+        self.quiz_canvas.create_image(0, 0, image=self.bg_image, anchor="nw")
+        window.update()
+        self.on_resize()
+
+        self.quiz_canvas.bind("<Configure>", self.on_resize)
+
+        
         window.update()
         print("done")
+
+    def on_resize(self, event=None):
+        self.bg_image = Image.open("resizedstarterpagev2.png")
+
+        ratio = self.quiz_canvas.winfo_width() / 634
+        print(ratio)
+        if 469 * ratio > self.quiz_canvas.winfo_height():
+            ratio = self.quiz_canvas.winfo_height() / 469
+        print(ratio)
+
+        if ratio <= 0:
+            return
+
+        #finding entry box place relative to size of window
+        width = int(634 * ratio)
+        height = int(469 * ratio)
+
+        if width == 0 or height == 0:
+            return
+
+        vertical_gap = (self.quiz_canvas.winfo_height() - height)/2
+        horizontal_gap = (self.quiz_canvas.winfo_width() - width)/2
+        button_ratio = int(height * 0.644 + vertical_gap)
+
+        self.bg_image = self.bg_image.resize((width, height), 1)
+        self.bg_image = ImageTk.PhotoImage(self.bg_image)
+        self.quiz_canvas.create_image(horizontal_gap, vertical_gap, image=self.bg_image, anchor="nw")
         
 
 #Starting Point of Quiz
