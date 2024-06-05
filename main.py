8ccdfd64da3a683ace547b9a4be32c30e0e83ca5
Dev-Maisuriya <168686445+Dev-Maisuriya@users.noreply.github.com>
2024-06-05 00:25:10 +0000

    added two buttons on starting page

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from PIL import Image, ImageTk
 #5D088E is the primary purple colour for my quiz images
 namesList = []
 asked = []
+secondary_colour = "#5D088E"
 
 class NameEnter:
     def __init__(self, parent):
         self.quiz_canvas = tk.Canvas(parent, bg="#290f42")
         self.quiz_canvas.pack(expand=True, fill=tk.BOTH)
 
-        bg_entry_colour = "#5D088E"
-        self.enter_name = tk.Entry(self.quiz_canvas, bg=bg_entry_colour, highlightthickness=0, foreground="#edd818", font=("Arial Narrow", 18, "bold"))
+        self.enter_name = tk.Entry(self.quiz_canvas, bg=secondary_colour, highlightthickness=0, foreground="#edd818", font=("Arial Narrow", 18, "bold"))
         self.enter_name.pack(anchor=tk.CENTER)
         
         self.bg_image = ImageTk.PhotoImage(Image.open("enternamepage.png"))
@@ -71,7 +71,13 @@ class StartPage:
         self.quiz_canvas = tk.Canvas(parent, bg="#290f42")
         self.quiz_canvas.pack(side="top", expand=True, fill=tk.BOTH)
         self.bg_image = ImageTk.PhotoImage(Image.open("resizedstarterpagev2.png"))
+        
+        self.start_button = tk.Button(self.quiz_canvas, bg=secondary_colour, text="Play", font=("Arial Narrow", 9, "bold"), activebackground="#69248f", highlightthickness=0, foreground="#edd818", width=20)
+        self.start_button.pack()
 
+        self.howtoplay_button = tk.Button(self.quiz_canvas, bg=secondary_colour, text="How To Play", font=("Arial Narrow", 9, "bold"), activebackground="#69248f", highlightthickness=0, foreground="#edd818", width=20)
+        self.howtoplay_button.pack()
+        
         self.quiz_canvas.create_image(0, 0, image=self.bg_image, anchor="nw")
         window.update()
         self.on_resize()
@@ -103,12 +109,22 @@ class StartPage:
 
         vertical_gap = (self.quiz_canvas.winfo_height() - height)/2
         horizontal_gap = (self.quiz_canvas.winfo_width() - width)/2
-        button_ratio = int(height * 0.644 + vertical_gap)
 
         self.bg_image = self.bg_image.resize((width, height), 1)
         self.bg_image = ImageTk.PhotoImage(self.bg_image)
         self.quiz_canvas.create_image(horizontal_gap, vertical_gap, image=self.bg_image, anchor="nw")
-        
+
+        #placing buttons relative to size of window
+        ratio2 = 290/469
+        button_location = int(height * ratio2)
+        self.start_button.place_configure(x=width/2 - self.start_button.winfo_width()/2, y=button_location)
+
+        ratio3 = 380/469
+        button_location2 = int(height * ratio3)
+        self.howtoplay_button.place_configure(x=width/2 - self.start_button.winfo_width()/2, y=button_location2)
+
+class HowToPlayPage:
+    def __init__(self, parent):
 
 #Starting Point of Quiz
 if __name__ == "__main__":
